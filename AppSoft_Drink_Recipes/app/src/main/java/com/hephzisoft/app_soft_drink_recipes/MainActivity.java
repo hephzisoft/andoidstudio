c515a926072bf7ed18d26c73d4431163e090662c
Hephzisoft <hephzisoft@gmail.com>
2022-12-13 10:36:44 +0100

    added the project for semester 3 exam

---
+++ AppSoft_Drink_Recipes/app/src/main/java/com/hephzisoft/app_soft_drink_recipes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hephzisoft.app_soft_drink_recipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
