9712500e3ee8ac2eadd133746c41e714c8ea436a
Hephzisoft <hephzisoft@gmail.com>
2022-12-18 01:24:52 +0100

    made some changes

---
+++ AppSoft_Drink_Recipes/app/src/main/java/com/hephzisoft/app_soft_drink_recipes/AllRecipeActivity.java
@@ -0,0 +1,14 @@
+package com.hephzisoft.app_soft_drink_recipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_recipe);
+    }
+}
