c752014acc398eaa5c57f7aa1b5859603304ac6e
Hephzisoft <hephzisoft@gmail.com>
2022-12-21 08:05:35 +0100

    still designing the home page
    and may God just put me through the thing

--- AppSoft_Drink_Recipes/app/src/main/java/com/hephzisoft/app_soft_drink_recipes/MainActivity.java
+++ AppSoft_Drink_Recipes/app/src/main/java/com/hephzisoft/app_soft_drink_recipes/MainActivity.java
@@ -1,8 +1,9 @@
 package com.hephzisoft.app_soft_drink_recipes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.taketohomepage).setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, AllRecipeActivity.class));
+        });
     }
 
 
