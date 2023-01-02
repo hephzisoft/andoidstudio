f8cbb1afd480a55a82c2672c9d8778a28d529d2a
Hephzisoft <hephzisoft@gmail.com>
2023-01-02 19:00:16 +0100

    made some changes

--- Semester3/app/src/main/java/com/hephzisoft/semester3/MainActivity.java
+++ Semester3/app/src/main/java/com/hephzisoft/semester3/MainActivity.java
@@ -1,22 +1,45 @@
 package com.hephzisoft.semester3;
 
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.taketohomepage).setOnClickListener(v -> {
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        });
+        new CountDownTimer(3000, 30) {
+
+            int progress = 0;
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                ++progress;
+                ((ProgressBar) findViewById(R.id.progressBar)).setProgress(progress);
+            }
+
+            @Override
+            public void onFinish() {
+                String[] db_data = new DBHelper(MainActivity.this).getLoggedInUser();
+
+                if (Integer.parseInt(db_data[0]) > 0) {
+                    startActivity(new Intent(MainActivity.this, HomePageActivity.class).putExtra("user", db_data[1]));
+                } else {
+                 startActivity(new Intent(MainActivity.this, HomePageActivity.class));
+                }
+
+                finish();
+            }
+        }.start();
+
 
     }
 }
